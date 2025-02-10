a5533d66b38a4d0b660572beb49d20e7f7405413
ricky-kiva <ricky.kiva@gmail.com>
2025-02-10 13:30:52 +0700

    feat(types): add `NonSensitiveDiaryEntry` type

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,8 @@
 export type Weather = 'sunny' | 'rainy' | 'cloudy' | 'windy' | 'stormy';
 export type Visiblity = 'great' | 'good' | 'ok' | 'poor';
 
+export type NonSensitiveDiaryEntry = Omit<DiaryEntry, 'comment'>;
+
 export interface DiaryEntry {
   id: number,
   date: string,
