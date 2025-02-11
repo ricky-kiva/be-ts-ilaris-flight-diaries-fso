0b60d173cabbb3506562a24f40b6cb47d955dec7
ricky-kiva <ricky.kiva@gmail.com>
2025-02-11 12:37:30 +0700

    feat(service): impl. add `diary` & find `diary` by id

--- src/routes/diaries.ts
+++ src/routes/diaries.ts
@@ -1,15 +1,55 @@
 import express, { Response } from 'express';
 import diaryService from '../services/diaryService';
-import { NonSensitiveDiaryEntry } from '../types';
+import { NewDiaryEntry, NonSensitiveDiaryEntry } from '../types';
 
-const r = express.Router();
+const diaryRouter = express.Router();
 
-r.get('/', (_req, res: Response<NonSensitiveDiaryEntry[]>) => {
+diaryRouter.get('/', (_req, res: Response<NonSensitiveDiaryEntry[]>) => {
   res.send(diaryService.getNonSensitiveEntries());
 });
 
-r.post('/', (_req, res) => {
-  res.send('Saving a diary!');
+diaryRouter.post('/', (req, res) => {
+  let newDiaryEntry: NewDiaryEntry;
+
+  try {
+    newDiaryEntry = req.body as NewDiaryEntry;
+
+    if (!newDiaryEntry.date || !newDiaryEntry.visibility || !newDiaryEntry.weather) {
+      res.sendStatus(404);
+      return;
+    }
+  } catch(e) {
+    res.sendStatus(404);
+    console.log(`Something's wrong: ${e}`);
+    
+    return;
+  }
+
+  const addedEntry = diaryService.addDiary(newDiaryEntry);
+
+  res.json(addedEntry);
 });
 
-export default r;
+diaryRouter.get('/:id', (req, res) => {
+  let id: number;
+
+  try {
+    id = Number(req.params.id);
+  } catch(e: unknown) {
+    res.sendStatus(404);
+    console.log(`Something's wrong: ${e}`);
+    
+    return;
+  }
+
+  const diary = diaryService.findById(Number(id));
+
+  if (!diary) {
+    res.sendStatus(404);
+    return;
+  }
+  
+  res.send(diary);
+});
+
+export default diaryRouter;
