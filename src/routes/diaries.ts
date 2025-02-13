143091f195fa6b84308f8b30b6fd2f865dc6755a
ricky-kiva <ricky.kiva@gmail.com>
2025-02-14 00:04:51 +0700

    feat(routes): impl. new `newDiaryEntry` with `zod`

--- src/routes/diaries.ts
+++ src/routes/diaries.ts
@@ -1,7 +1,8 @@
 import express, { Response } from 'express';
 import diaryService from '../services/diaryService';
 import { NewDiaryEntry, NonSensitiveDiaryEntry } from '../types';
-import toNewDiaryEntry from '../utils';
+import { toNewDiaryEntry } from '../utils';
+import z from 'zod';
 
 const diaryRouter = express.Router();
 
@@ -19,9 +20,13 @@ diaryRouter.post('/', (req, res) => {
       res.sendStatus(404);
       return;
     }
-  } catch(e) {
-    res.sendStatus(404);
-    console.log(`Something's wrong: ${e}`);
+  } catch(e: unknown) {
+    if (e instanceof z.ZodError) {
+      res.status(400).send({ error: e.issues });
+      return;
+    }
+
+    if (e instanceof Error) res.status(400).send({ error: e.message }); 
     
     return;
   }
