c048d632ff8717818c8bd686fc291a8937989451
ricky-kiva <ricky.kiva@gmail.com>
2025-02-14 00:25:32 +0700

    refactor(utils)!: set `NewEntrySchema` title case & rmv. `toNewDiaryEntry`

--- src/utils.ts
+++ src/utils.ts
@@ -1,13 +1,9 @@
-import { NewDiaryEntry, Visiblity, Weather } from "./types";
+import { Visiblity, Weather } from "./types";
 import z from 'zod';
 
-const newEntrySchema = z.object({
+export const NewEntrySchema = z.object({
   weather: z.nativeEnum(Weather),
   visibility: z.nativeEnum(Visiblity),
   date: z.string().date(),
   comment: z.string().optional()
 });
-
-export const toNewDiaryEntry = (object: unknown): NewDiaryEntry => {
-  return newEntrySchema.parse(object);
-};
